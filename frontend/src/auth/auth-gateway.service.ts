e6d17ea072f4cf078deaf7f97a9f20d31abaa718
Viacheslav Popov <viacheslav.popov@horus.com.pl>
2025-03-26 17:08:48 +0100

    front register component, togo login. Environments. Prettier for formatting. Readme update

---
+++ frontend/src/auth/auth-gateway.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import * as bcrypt from 'bcryptjs';
+import { environment } from '../environment/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGatewayService {
+  private apiUrl = `${environment.apiUrl}/register`;
+
+  constructor(private http: HttpClient) {
+  }
+
+  // TODO: login
+
+  register(email: string, password: string): Observable<any> {
+    const saltRounds = 10;
+    const hashedPassword = bcrypt.hashSync(password, saltRounds);
+
+    return this.http.post(this.apiUrl, {
+      email,
+      password: hashedPassword,
+    });
+  }
+}
---
+++ frontend/src/environment/environment.ts
@@ -0,0 +1,5 @@
+// We are replacing this file for local build. Notice fileReplacements section in angular.json
+export const environment = {
+  local: false,
+  apiUrl: 'idk yet',
+};
