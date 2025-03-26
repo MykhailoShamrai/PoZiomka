c33eb3a5b8603825e84a122d9b2325b20f975cdb
chefxxx <mateusz.mm100@gmail.com>
2025-03-26 10:50:55 +0100

    init mati 2

--- backend/Controllers/AdminController.cs
+++ backend/Controllers/AdminController.cs
@@ -4,25 +4,29 @@ public class AdminController
     private readonly CommunicationSender _communicationSender;
     private readonly RoomSelector _roomSelector;
 
-    public AdminController(ApplicationService applicationService, CommunicationSender communicationSender, RoomSelector roomSelector)
-    {
-        _applicationService = applicationService;
-        _communicationSender = communicationSender;
-        _roomSelector = roomSelector;
-    }
-
-    public void MakeApplication(string type)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void SendCommunication(Communication communication, List<User> users)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void ResolveAnApplication(Application application)
-    {
-        throw new NotImplementedException();
-    }
+    
+    
+    // public AdminController(ApplicationService applicationService, CommunicationSender communicationSender, RoomSelector roomSelector)
+    // {
+    //     _applicationService = applicationService;
+    //     _communicationSender = communicationSender;
+    //     _roomSelector = roomSelector;
+    // }
+    //
+    // public void MakeApplication(string type)
+    // {
+    //     throw new NotImplementedException();
+    // }
+    //
+    // public void SendCommunication(Communication communication, List<User> users)
+    // {
+    //     throw new NotImplementedException();
+    // }
+    //
+    // public void ResolveAnApplication(Application application)
+    // {
+    //     throw new NotImplementedException();
+    // }
+    
+    //TODO: w controllerach siedzą endpointy, injectujemy serwisy i jazda
 }
--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -3,5 +3,4 @@ public class User
     public int Id { get; set; }
     public string Email { get; set; } = string.Empty;
     public string Phone { get; set; } = string.Empty;
-
 }
