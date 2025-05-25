e75397280c825969f5e00e6bc6237725ddd812d9
MykhailoShamrai <99137234+MykhailoShamrai@users.noreply.github.com>
2025-05-26 00:57:26 +0200

    Update environment.ts again

--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -1,5 +1,5 @@
 // We are replacing this file for local build. Notice fileReplacements section in angular.json
 export const environment = {
   local: false,
-  apiUrl: 'poziomka-hkfjd6eqfybsazbx.polandcentral-01.azurewebsites.net/api/',
+  apiUrl: 'https://poziomka-hkfjd6eqfybsazbx.polandcentral-01.azurewebsites.net/api/',
 };
