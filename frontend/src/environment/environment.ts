607f1c8a031dcc529c7ff11d4f121f3e78d5a08d
Mykhailo Shamrai <mike.shamrai@gmail.com>
2025-05-25 19:42:43 +0200

    Check with production env

--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -1,5 +1,5 @@
 // We are replacing this file for local build. Notice fileReplacements section in angular.json
 export const environment = {
   local: false,
-  apiUrl: 'idk yet/',
+  apiUrl: 'https://poziomka-hkfjd6eqfybsazbx.polandcentral-01.azurewebsites.net/api/',
 };
