6d461c1ba0b90ae85be094e56c5b140808155882
Mykhailo Shamrai <mike.shamrai@gmail.com>
2025-05-25 19:46:56 +0200

    Added frontend to CORS

--- backend/Program.cs
+++ backend/Program.cs
@@ -100,7 +100,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularApp", policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
+        policy.WithOrigins("http://localhost:4200", "https://kind-water-0f68c7f03.6.azurestaticapps.net")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
